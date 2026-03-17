bb04107145b31f8d77dd133afd3a2444193ea5a9
ganeshdevops66 <ganeshdevops66@gmail.com>
2026-03-17 19:21:22 +0530

    Reorganize and restore AWS resources in main.tf

--- main.tf
+++ main.tf
@@ -70,14 +70,14 @@ resource "aws_s3_bucket" "mybucket1" {
 
 resource "aws_instance" "webserver1" {
   ami                    = "ami-0b6c6ebed2801a5cb"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.mysg.id]
   subnet_id              = aws_subnet.sub1.id
   user_data              = base64encode(file("userdata.sh"))
 }
 resource "aws_instance" "webserver2" {
   ami                    = "ami-0b6c6ebed2801a5cb"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.mysg.id]
   subnet_id              = aws_subnet.sub2.id
   user_data              = base64encode(file("userdata1.sh"))
